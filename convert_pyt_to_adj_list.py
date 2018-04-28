fad2b400fa5ba07e8ec996f82bab9d8a95fda608
Naren Dasan <naren@narendasan.com>
2018-04-28 17:46:52 -0500

    simple mlp upload

---
+++ convert_pyt_to_adj_list.py
@@ -0,0 +1,25 @@
+import torch
+
+def convert_sd_to_adj(name, state_dict):
+    keys = state_dict.keys()
+    weights = []
+    biases = []
+    for k in keys:
+        if "weight" in k:
+            weights.append(k)
+        elif "bias" in k:
+            biases.append(k)
+    
+    edges = []
+    in_dim_start = 0
+    for w in weights:
+        edge_weights = state_dict[w].numpy()
+        edge_weights = edge_weights.T
+        for i in range(edge_weights.shape[0]):
+            in_dim = in_dim_start + i
+            for j in range(edge_weights.shape[1]):
+                out_dim = in_dim_start + edge_weights.shape[0] + j
+                edges.append((in_dim, out_dim, edge_weights[i][j]))
+        in_dim_start = in_dim_start + edge_weights.shape[0]
+                
+    open(name + '.adj', 'w').write('\n'.join('%s %s %s' % x for x in edges))
